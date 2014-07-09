eae8c0d8704e94446a735433bd3c52607af03615
brad <brad@105d77e0-32f4-4e0e-9b95-44c0bbbba7b3>
2014-07-09 17:36:12 +0000

    initial

---
+++ dvid/copy.sh
@@ -0,0 +1,7 @@
+cp ../../robotron/rtl/dvid2/DRAM16XN.v .
+cp ../../robotron/rtl/dvid2/serdes_5_to_1.v .
+cp ../../robotron/rtl/dvid2/encode.v .
+cp ../../robotron/rtl/dvid2/convert_30to15_fifo.v .
+cp ../../robotron/rtl/dvid2/dvid.v .
+cp ../../robotron/rtl/dvid2/dvid_output.v .
+
